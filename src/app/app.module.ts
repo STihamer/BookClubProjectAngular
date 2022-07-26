faeddd8c9e367d35c519000b8715e15cbe387887
Tihamer Sebok <tihamer.sebok@endava.com>
2022-07-26 10:14:12 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { UsersComponent } from './users/users.component';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UsersComponent,
+    BooksComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
